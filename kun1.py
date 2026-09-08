26e10ad51ee5725b38759b397cf66795e478f4ce
MukhiddinovaDurdona <medurdoniy@gmail.com>
2026-09-08 20:31:14 +0500

    .

---
+++ kun1.py
@@ -0,0 +1,14 @@
+def count_vowels_and_consonants(text):
+    vowels = set("aeiouAEIOU")
+    result = {"unli": 0, "undosh": 0}
+    
+    for char in text:
+        if char.isalpha():
+            if char in vowels:
+                result["unli"] += 1
+            else:
+                result["undosh"] += 1
+                
+    return result
+soz = input ("soz kiriting : ")
+print (count_vowels_and_consonants(soz))
